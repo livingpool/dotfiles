0cd6029eb1605bc2e955094e2ec88bd889e78a0e
livingpool <52132459+livingpool@users.noreply.github.com>
2026-08-01 11:18:36 +0800

    fix some bugs for linux mint (like ubuntu 24.04 has only old fzf versions)

--- install-after-chezmoi-linuxmint.sh
+++ install-after-chezmoi-linuxmint.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# add a link to fd
+# add a symlink to fd; should only be executed once
 ln -s $(which fdfind) ~/.local/bin/fd
+# add a symlink to bat; should only be executed once
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+# install diff-so-fancy (debian)
+sudo add-apt-repository ppa:aos1/diff-so-fancy
+sudo apt update
+sudo apt install diff-so-fancy
+
+# install bat themes
+mkdir -p "$(bat --config-dir)/themes"
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+bat cache --build
 
 # install rustup, cargo, cargo binstall, tree-sitter-cli
 curl https://sh.rustup.rs -sSf | sh
